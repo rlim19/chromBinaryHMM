da4514a5f5bf2678419ad2743489a0184f1f1c30
rickylim19 <rickylim19@gmail.com>
2013-04-17 18:51:27 +0200

    make math notations more human friendly

--- ForwardBackward.py
+++ ForwardBackward.py
@@ -1,104 +1,111 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
-###########################
-# emission : binary (0/1) #
-###########################
+################################################################################################
+# forward-backward: compute probability of being in state k, at time t given the obs.sequence  #
+# emission : binary (0/1)                                                                      #
+################################################################################################
 
-import unittest
 import numpy as np
 import math
 import os
+import unittest
 import numpy.testing as npt
 
-
-def forward_backward(o, z, q, e, a, elem_size=np.longdouble):
+def forward_backward(obs, no_states, trans, emm, a0, elem_size=np.longdouble):
    """
-   o : observed data
-   z : no.hidden states
-   q : transition probs
-   e : emission prob
-   a : initial probs
+   obs       : observed data
+   no_states : no.hidden states
+   trans     : transition probs
+   emm       : emission prob
+   a0        : initial probs
 
+   output:
+   - phi matrix, the probability of being in state k, at each t step
+     given the obs.sequence
    """
 
+   # for debugging
    #import pdb; pdb.set_trace()
-   # forward part
-   alpha = np.zeros((z, len(o)), dtype=elem_size)
+
+   ################
+   # forward part #
+   ################
+
+   # create the array containing zeros
+   # rows are states
+   # cols are the time step of the obs.sequence
+   alpha = np.zeros((no_states, len(obs)), dtype=elem_size)
 
    scale_const = list()
-   # iterate by columns
-   for i, x_i in enumerate(o):
+   # iterate over the obs.sequence
+   for t, x_t in enumerate(obs):
 
-      for state in range(0,z):
-         if i == 0:
-            prev_fsum = a[state]
+      for state in range(no_states):
+         if t == 0:
+            prev_fsum = a0[state]
          else:
-            prev_fsum = sum(alpha[k,i-1]*q[k,state] for k in range(0,z))
-         alpha[state,i] = (e[state, x_i] * prev_fsum)
+            prev_fsum = sum(alpha[k,t-1]*trans[k,state] for k in range(no_states))
+         alpha[state,t] = (emm[state, x_t] * prev_fsum)
 
       # normalize alpha by rowsums -> with a total alpha for each time: 1.0
       # normalization (by division) to avoid overflow due to multiplication of small numbers!
-      statesum = sum([alpha[state,i] for state in range(z)])
-      for state in range(z):
-         alpha[state,i] = alpha[state,i] / statesum
+      statesum = sum([alpha[state,t] for state in range(no_states)])
+      for state in range(no_states):
+         alpha[state,t] = alpha[state,t] / statesum
       scale_const.append(statesum)
 
-   prob_fw = sum(alpha[:,-1])
+   #prob_fw = sum(alpha[:,-1])
 
-   print "prob_fw:" + str(prob_fw)
-   print "Alpha Matrix:"
-   print alpha
+   #################
+   # backward part #
+   #################
+   beta = np.zeros((no_states, len(obs)),dtype=elem_size)
 
+   # fill backward from the last obs.sequence
+   for state in range(no_states):
+      beta[state, len(obs)-1] = 1
 
-   # backward part
-   beta = np.zeros((z, len(o)),dtype=elem_size)
-   for s in range(z):
-      beta[s, len(o)-1] = 1
-   for t in range(len(o)-2,-1,-1):
-      for i in range(z):
-         for j in range(z):
-            beta[i, t] += q[i,j] * e[j][o[t+1]] * beta[j,t+1]
+   # t is the time step of the obs.sequence
+   for t in range(len(obs)-2,-1,-1):
+      # transition from i[t] to j[t+1] 
+      for i in range(no_states):
+         for j in range(no_states):
+            beta[i, t] += trans[i,j] * emm[j][obs[t+1]] * beta[j,t+1]
 
-      for state in range(z):
+      for state in range(no_states):
          # normalize with scaling constant from forward mat (alpha)
          beta[state, t] = beta[state, t]/scale_const[t+1]
 
    # double check, the prob of bw and fw should be the same
-   prob_bw = sum(a[state] * e[state, o[0]] * beta[state,0]for state in range(0,z))
-   print "prob_bw:" + str(prob_bw[0])
-   print "Beta Matrix:"
-   print beta
+   #prob_bw = sum(a0[state] * emm[state, obs[0]] * beta[state,0]for state in range(no_states))
 
    # forward/backward
    phi = (alpha * beta)
-   print "Phi Matrix:"
-   print phi
    return phi
 
 class TestFwdBwd(unittest.TestCase):
    def test_fwdbw(self):
       # for test purpose (Toy example)
       # get the obs.data, returned as np array objects
-      o = np.genfromtxt("profile2.test", names=True, delimiter="\t")
-      o = o['001']
+      obs = np.genfromtxt("profile2.test", names=True, delimiter="\t")
+      obs = obs['001']
 
       # random_transition  = np.random.uniform(0,1, size = state*state).reshape(state,state)
       # e.g transition, row and col index start with 0
-      q = np.array([[0.8,0.2], [0.5,0.5]])
+      trans = np.array([[0.8,0.2], [0.5,0.5]])
 
       # random_emission = np.random.uniform(0,1, size = 2*state).reshape(state,2)
-      e = np.array([[0.2,0.8], [0.7,0.3]])  #binary emission
+      emm = np.array([[0.2,0.8], [0.7,0.3]])  #binary emission
 
       # random init_prob = np.random.uniform(0,1,size=1*state).reshape(state,1)
-      a = np.array([[0.5],[0.5]])
+      a0 = np.array([[0.5],[0.5]])
 
-      phi_mat = forward_backward(o, 2, q, e, a)
+      phi_mat = forward_backward(obs, 2, trans, emm, a0)
       target_phi = np.array([[0.65753425, 0.48097412, 0.81722114], [0.34246575, 0.51902588, 0.18277886]])
-      prob_fw = 0.11497500000000001373
+      
       # compare two numpy arrays
       npt.assert_almost_equal(phi_mat, target_phi, decimal = 7)
-
 
 if __name__== '__main__':
    unittest.main()
